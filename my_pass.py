96bbe07c0c947df08c40f857a0ddb7d2e17ce0d4
vonrimS <mr.mytab@gmail.com>
2022-06-17 00:58:56 +0200

    Search functionality added

--- my_pass.py
+++ my_pass.py
@@ -1,13 +1,52 @@
+from json import JSONDecodeError
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
 import json
 import os
-
-# ----- PASSWORD GENERATOR -----
 import string
 import random
+import re
 
+FILE_NAME = 'data.json'
+
+# ----- CHECK FILE EXISTENCE -----
+def create_if_not_exist(file_name):
+    """Check json file existence. If not found, it creates a new one."""
+    if not os.path.exists(file_name):
+        f = open(file_name, 'x')
+        f.close()
+        print(f"{FILE_NAME} was created successfully.")
+
+# ----- SEARCHING PASSWORD -----
+def search():
+    """Look for the user input in a website field and, if record exists,
+     get back email/username and password."""
+    website = website_input.get().lower()
+    if website == '':
+        messagebox.showinfo(title="...something goes wrong",
+                            message="Please fill website form before clicking search button!")
+    elif os.stat(FILE_NAME).st_size == 0:
+        messagebox.showinfo(title="...something goes wrong",
+                            message="Password repo is empty!")
+    else:
+        f = open(FILE_NAME, 'r')
+        data = json.load(f)
+        found = 0
+        for key in data:
+            if website in key:
+                print(key)
+                messagebox.showinfo(title="Search results",
+                                    message=f"email:\n\t{data[key]['email']}\n\n"
+                                            f"password:\n\t{data[key]['password']}")
+                found = 1
+
+        if found == 0:
+            messagebox.showinfo(title="Search results",
+                                message="...nothing according to this search input")
+
+
+# ----- PASSWORD GENERATOR -----
 def generate_pass(pass_len=15):
     upper_letters = string.ascii_uppercase
     lower_letters = string.ascii_lowercase
@@ -26,12 +65,11 @@ def generate_pass(pass_len=15):
 
 
 # ----- VALIDATE EMAIL ADDRESS -----
-import re
-
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 def isValid(email):
-    """ Check email according to regex template. Return False if it's not match the regex expression.
+    """ Check email according to regex template.
+    Return False if it's not match the regex expression.
     """
     email = ''.join(email)
     match = re.fullmatch(regex, email)
@@ -47,7 +85,6 @@ def save_password():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
-    file_name = "data.json"
     input_ok = check_inputs(website, email, password)
 
     if input_ok:
@@ -60,24 +97,19 @@ def save_password():
         }
         # Temporally assign new_data to data
         data = new_data
-        # Check if file has some content
-        try:
-            f = open(file_name, 'r')
-        except FileNotFoundError:
-            f = open(file_name, 'w')
-            json.dump(data, f, indent=4)
-        else:
-            if os.stat(file_name).st_size != 0:
-                # If file is empty then reassign found file deserialized content to 'data' variable
-                data = json.load(f)
-                data.update(new_data)
-                f.close()
-            f = open(file_name, 'w')
-            json.dump(data, f, indent=4)
 
-        finally:
-            clean_inputs()
+        # Check if file has some content
+        if os.stat(FILE_NAME).st_size != 0:
+            f = open(FILE_NAME, 'r')
+            # If file is empty then reassign found file deserialized content to 'data' variable
+            data = json.load(f)
+            data.update(new_data)
             f.close()
+        f = open(FILE_NAME, 'w')
+        json.dump(data, f, indent=4)
+
+        clean_inputs()
+        f.close()
 
 
 # ----- CLEAN INPUT FIELDS -----
@@ -107,6 +139,9 @@ window.title("Password Manager")
 window.config(bg="white")
 window.config(padx=50, pady=50)
 
+# Check if json file exists
+create_if_not_exist(FILE_NAME)
+
 # App logo canvas
 canvas = Canvas(width=350, height=253, bg="white", highlightthickness=0)
 lock_img = PhotoImage(file="lock.png")
@@ -124,8 +159,8 @@ password_label = Label(text="Password", bg="white")
 password_label.grid(row=3, column=0, padx=10, sticky='e')
 
 # Entries
-website_input = Entry(width=40)
-website_input.grid(row=1, column=1, columnspan=2, sticky='w', pady=10)
+website_input = Entry(width=18)
+website_input.grid(row=1, column=1, sticky='w', pady=10)
 website_input.focus()
 
 email_input = Entry(width=40)
@@ -136,7 +171,9 @@ password_input = Entry(width=18)
 password_input.grid(row=3, column=1, sticky='w', pady=10)
 
 # Buttons
-gen_button = Button(text="Generate Password", command=generate_pass)
+search_button = Button(text="Search", command=search, width=15)
+search_button.grid(row=1, column=2, pady=10, sticky='e')
+gen_button = Button(text="Generate Password", command=generate_pass, width=15)
 gen_button.grid(row=3, column=2, pady=10, sticky='e')
 add_button = Button(text="Add", width=34, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky='w', pady=10)
